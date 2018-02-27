d7c8689a9830221d93b0fe72da6725f562f381a6
RGero215 <rgero215@gmail.com>
2018-02-27 19:54:51 -0400

    assignment: display contact avatar, name, email and remove button
    Pass data with props from contacts array temporary.  Eventually, we'll be retrieving and storing contacts on our backend server.

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
 
-
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
 
 
 
@@ -8,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts}/>
       </div>
     )
   }
