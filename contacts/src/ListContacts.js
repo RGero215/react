a7b3367852aec0f901a8910fe3061c202c2dc3ac
RGero215 <rgero215@gmail.com>
2018-03-04 17:31:21 -0400

    modified: ListContacts.js
    Filter and sort the contacts that are searched for.

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
     static propTypes = {
@@ -15,6 +17,16 @@ class ListContacts extends Component {
         this.setState({query: query.trim()})
     }
     render() {
+        let showingContacts
+        if(this.state.query){
+            const match = new RegExp(escapeRegExp(this.state.query), 'i')
+            showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+        } else {
+            showingContacts = this.props.contacts
+        }
+
+        showingContacts.sort(sortBy('name'))
+        
         return (
             <div className='list-contacts'>
                 <div className='list-contacts-top'>
@@ -26,7 +38,7 @@ class ListContacts extends Component {
                     />
                 </div>
                 <ol className='contact-list'>
-                    {this.props.contacts.map((contact) => (
+                    {showingContacts.map((contact) => (
                         <li key={contact.id} className='contact-list-item'>
                             <div className='contact-avatar' style={{
                                 backgroundImage: `url(${contact.avatarURL})`
